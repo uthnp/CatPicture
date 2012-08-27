c72ac0749d388c101c29d75c09cfb129376abbad
Nicholas Uth <uthnp@muohio.edu>
2012-08-27 06:48:44 -0400

    Added color shifting

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -5,16 +5,25 @@ using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+// the : below == extends
 class CatPictureApp : public AppBasic {
-  public:
+  public: // this means everything below this is public until we set to private
 	void setup();
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+
+  private:
+	float red;
+	float green;
+	float blue;
 };
 
 void CatPictureApp::setup() //for when program starts
 {
+	red = 0.0;
+	green = 0.0;
+	blue = 0.0;
 }
 
 void CatPictureApp::mouseDown( MouseEvent event )
@@ -23,12 +32,27 @@ void CatPictureApp::mouseDown( MouseEvent event )
 
 void CatPictureApp::update() //updates variables
 {
+	red = red + 0.01f;
+	if (red > 1.0f)
+	{
+		red = 0;
+	}
+	green = green + 0.02f;
+	if (green > 1.0f)
+	{
+		green = 0;
+	}
+	blue = blue + 0.03f;
+	if (blue > 1.0f)
+	{
+		blue = 0;
+	}
 }
 
 void CatPictureApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); //from 0 to 1
+	gl::clear( Color( red, green, blue ) ); //from 0 to 1
 }
 
 CINDER_APP_BASIC( CatPictureApp, RendererGl )
